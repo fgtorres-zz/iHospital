98e666584f94d55cf198de72238c27ef5e64a9bc
Rafael Melo <rsmelo_@hotmail.com>
2018-06-27 12:13:37 -0300

    Trying to add main screen

--- map-app/App.js
+++ map-app/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Main from './src/screens/Main';
 import { View } from 'react-native';
+import { Header } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
+        <Header
+          leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'iHospital', style: { color: '#fff' } }}
+          rightComponent={{ icon: 'home', color: '#fff' }}
+        />
         <Main />
       </View>
     );
--- map-app/src/screens/Main.js
+++ map-app/src/screens/Main.js
@@ -1,44 +1,60 @@
 import React from 'react';
-import { ScrollView, View, StyleSheet, Text } from 'react-native';
-import Map from '../components/Map';
+import { FlatList, View, StyleSheet, Dimensions } from 'react-native';
+import { Avatar, Text, SearchBar } from 'react-native-elements';
 
-
+const { height } = Dimensions.get('window');
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  containerSearch: {
+    backgroundColor: 'transparent',
+    borderTopColor: 'transparent',
+    marginBottom: 20,
   },
-  boxSmall: {
-    width: 200,
-    height: 200,
-    marginBottom: 10,
-    marginRight: 10,
-    backgroundColor: 'skyblue',
+  inputSearch: {
+    backgroundColor: '#eee',
+    marginHorizontal: 10,
+    marginBottom: 12,
   },
-  boxLarge: {
-    width: 300,
-    height: 300,
-    marginBottom: 10,
-    marginRight: 10,
-    backgroundColor: 'steelblue',
+  scrollView: {
+    backgroundColor: '#000',
+    paddingHorizontal: 10,
+    // height: '100%',
   },
 });
 
+const items = [
+  {key: '1', name: 'Rafael'},
+  {key: '2', name: 'Melo'},
+]
+
+
+
 export default class Main extends React.Component {
+  _renderItem (item){
+    return (
+      <Text>{item.name}</Text>
+    )
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Map />
-        {/* <ScrollView  style={styles.container}>
-          <View style={styles.boxLarge} />
-          <ScrollView horizontal>
-            <View style={styles.boxSmall} />
-            <View style={styles.boxSmall} />
-            <View style={styles.boxSmall} />
-          </ScrollView>
-          <View style={styles.boxLarge} />
-          <View style={styles.boxSmall} />
-          <View style={styles.boxLarge} />
-        </ScrollView> */}
+        <Text style={{ paddingHorizontal: 10 }} h3>Hospitais de Salvador</Text>
+        <SearchBar
+          containerStyle={styles.containerSearch}
+          inputStyle={styles.inputSearch}
+          lightTheme
+          round
+          showLoading
+          // searchIcon={false} // You could have passed `null` too
+          // onChangeText={someMethod}
+          // onClear={someMethod}
+          placeholder='Busque pelo nome'
+        />
+        <Text h4>Hospitais de Salvador</Text>
+
+        <FlatList
+          renderItem={ item => this._renderItem(item) }
+          data={items}
+        />
       </View>
 
     )
